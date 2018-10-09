c0995cd9f903304db7c38547a57ad50acbaee9d7
Giang Tran <giangtt@forixwebdesign.com>
2018-10-09 16:50:28 +0700

    change html slide image

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,25 @@ $(document).ready(function() {
         autoplay: false,
     });
 
+    //slide images ====
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true,
+        vertical: true,
+    });
+
     //scroll menu link======
     //Scroll
 
